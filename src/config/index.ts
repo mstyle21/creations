ea5b6d82d4ce9c89a8e68a0ced5211ff6a562e74
mihai <mr.r.mihai@gmail.com>
2023-12-08 22:40:54 +0200

    clean up package.json + removed lock from git

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,11 +1,11 @@
-export const BACKEND_URL = "http://192.168.100.26:7000";
+export const BACKEND_URL = "http://192.168.100.27:7000";
 export const CURRENCY_SIGN = "RON";
 export const MEASURE_SIGN = "cm";
 //estimated material costs
 export const MATERIALS_PRICES_PER_ITEM = {
-  //material cost per 1g RON
-  plaster: 0.003,
-  //material cost per 13 cm x 20 cm RON
+  //material cost per 1g
+  plaster: 0.005,
+  //material cost per 13 cm x 20 cm
   bubbleWrap: 0.1,
   scotch: 0.1,
 };
